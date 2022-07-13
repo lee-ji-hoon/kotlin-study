d83ce44d12e98a5506174ff8ecac551355644592
lee-ji-hoon <dlwlgns1240@naver.com>
2022-07-13 15:27:02 +0900

    [부스트코스] 1장 코드 정리

---
+++ kotlin_based/chap02/section3/AnyTest2.kt
@@ -0,0 +1,13 @@
+package chap02.section3
+
+fun main() {
+    checkArg("Hello") // 문자열을 인자로 넣음
+    checkArg(5) // 숫자를 인자로 넣음
+}
+
+fun checkArg(s: Any) {
+    if(s is String)
+        println("x is String : $s")
+    else if(s is Int)
+        println("x is Int : $s")
+}
---
+++ kotlin_based/chap02/section4/InDecOp.kt
@@ -0,0 +1,12 @@
+package chap02.section4
+
+fun main() {
+    var a: Int = 10
+    var b: Int = 10
+
+    var result1 = ++a // 증가 후에 대입
+    var result2 = b++ // 대입 후에 증가
+
+    println("result1 : $result1, result2 : $result2") // result1 : 11, result2 : 10
+    println("a : $a, b : $b") // a : 11, b : 11
+}
