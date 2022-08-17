9c564df37b0d25f49fc739e5b69ec1f785fd6776
lee-ji-hoon <dlwlgns1240@naver.com>
2022-08-17 14:41:08 +0900

    Refactor: 함수 분할 및 nickname 로직 변경

--- kotlin-playground/section03/Practice01.kt
+++ kotlin-playground/section03/Practice01.kt
@@ -2,7 +2,6 @@ package section03
 
 import section03.category.*
 
-// 1. 카테고리 정의
 enum class category(
     val koreanName: String
 ) {
@@ -13,7 +12,7 @@ enum class category(
     override fun toString() = koreanName
 }
 
-// 2. 장바구니용 배열 만들기
+
 val shoppingCart = mutableMapOf<category, Int>()
 
 fun main() {
@@ -24,12 +23,13 @@ fun main() {
         """.trimIndent()
     println(greetings)
     try {
-        val nickname: String = readLine() ?: throw Exception("이름이 입력되지 않았습니다.")
-        println("""
-            감사합니다. 반값습니다, ${nickname} 님
-            원하시는 카테고리를 입력해주세요.
-            =====================================
-            """.trimIndent())
+        val nickname: String? = readLine()
+        if (nickname.isNullOrEmpty()) throw Exception("이름이 입력되지 않았습니다.")
+        println("감사합니다. 반값습니다, ${nickname} 님")
+        // 1. 카테고리 정의
+        // 2. 장바구니용 배열 만들기
+        // 3. 장바구니 선택 when
+        // 4.. # 입력하면 장바구니로 이동
         showCategory()
     } catch (e: Exception) {
         e.printStackTrace()
@@ -37,24 +37,30 @@ fun main() {
 }
 
 fun showCategory() {
+    println("""
+        =====================================
+        원하시는 카테고리를 입력해주세요.
+        =====================================
+    """.trimIndent())
     for (value in values())
         if (value.toString() != "#") println(value.toString())
+    println("=====================================")
     choiceCategory()
 }
 
 fun choiceCategory() {
     println("-> 장바구니로 이동하시려면 #을 입력해주세요.")
-    // 3. #이나 다른 것을 입력할 때까지 반복
     while (true) {
         val inputProduct = readln()
-        // 4. 장바구니 선택 when
         when (inputProduct) {
             DEVICES.koreanName -> shoppingCart.put(DEVICES, shoppingCart.getOrDefault(DEVICES, 0) + 1)
             PASSION.koreanName -> shoppingCart.put(PASSION, shoppingCart.getOrDefault(PASSION, 0) + 1)
             PET_SUPPLIES.koreanName -> shoppingCart.put(PET_SUPPLIES, shoppingCart.getOrDefault(PET_SUPPLIES, 0) + 1)
-            // 3. # 입력하면 장바구니로 이동
             CART.koreanName -> cart()
-            else -> println("존재하지 않는 카테고리를 입력했습니다. 입력값 -> ${inputProduct}")
+            else -> {
+                println("존재하지 않는 카테고리를 입력했습니다. 입력값 -> ${inputProduct}")
+                showCategory()
+            }
         }
     }
 }
