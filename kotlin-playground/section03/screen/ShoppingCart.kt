35237286937697e160aaf76f8ca4c59e74d00da2
lee-ji-hoon <dlwlgns1240@naver.com>
2022-08-17 23:06:25 +0900

    Feat: ShoppingCart 이전 화면 이동, 기능 추가

--- kotlin-playground/section03/screen/ShoppingCart.kt
+++ kotlin-playground/section03/screen/ShoppingCart.kt
@@ -2,23 +2,23 @@ package section03.screen
 
 import section03.LINE_DIVIDER
 import section03.data.CartItems
+import section03.extensions.getNotEmptyInt
+import section03.extensions.getNotEmptyString
 
 class ShoppingCart : Screen() {
     private val products = CartItems.products
     fun showCart() {
         ScreenStack.push(this)
         if (products.isNotEmpty()) {
-            println(
-                products.keys.joinToString(
-                    separator = ", \n",
-                    prefix = """
+            println(products.keys.joinToString(
+                separator = ", \n", prefix = """
                     ${LINE_DIVIDER}
                     장바구니에 담은 상품 목록입니다.
+                    
                     """.trimIndent()
-                ) { product ->
-                    "카테고리 : ${product.category} / 상품명 : ${product.name} / 수량 : ${products[product]} / 1개 가격 : ${product.price}"
-                }
-            )
+            ) { product ->
+                "카테고리 : ${product.category} / 상품명 : ${product.name} / 수량 : ${products[product]} / 1개 가격 : ${product.price}"
+            })
         } else {
             println(
                 """
@@ -26,5 +26,32 @@ class ShoppingCart : Screen() {
             """.trimIndent()
             )
         }
+        showPreviousScreenOption()
+    }
+
+    private fun showPreviousScreenOption() {
+        println(
+            """
+            ${LINE_DIVIDER}
+            이전 화면으로 돌아가시겠습니까? (y/n)
+            """.trimIndent()
+        )
+        when (readln().getNotEmptyString()) {
+            "y", "Y" -> moveToPreviousScreen()
+            "n", "N" -> showCart()
+            else -> {}
+        }
+    }
+
+    private fun moveToPreviousScreen() {
+        ScreenStack.pop()
+        when (val previousScreen = ScreenStack.peek()){
+            is ShoppingCategory -> previousScreen.showCategory()
+            is ShoppingProductList -> previousScreen.showProducts()
+            is ShoppingCart, is ShoppingMainHome -> {
+                // 아무것도 하지 않는다.
+            }
+        }
+
     }
 }
