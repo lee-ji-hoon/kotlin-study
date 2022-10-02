539e29f1bb57902355fd92de5b05e1a0ce19e81b
lee-ji-hoon <dlwlgns1240@naver.com>
2022-10-02 15:15:24 +0900

    Feat: sealed example

---
+++ etc/selead-class/src/sealedExample/Expr.kt
@@ -0,0 +1,21 @@
+package sealedExample
+
+sealed class Expr
+
+data class Const(val number: Double) : Expr()
+data class Sum(val e1: Expr, val e2: Expr) : Expr()
+object NotANumber : Expr()
+
+fun main() {
+    val num1 = Const(10.0)
+    val num2 = Const(20.0)
+    val sum = Sum(num1, num2)
+    val result = eval(sum)
+    println("result : $result")
+}
+
+fun eval(expr: Expr): Double = when(expr) {
+    is Const -> expr.number
+    is Sum -> eval(expr.e1) + eval(expr.e2)
+    is NotANumber -> Double.NaN
+}
