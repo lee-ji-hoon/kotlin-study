0a1c563f2fd9eae36520891d750e60e6f0929384
lee-ji-hoon <dlwlgns1240@naver.com>
2023-01-21 15:37:38 +0900

    feat: Volatile 테스트 주석 내용 추가

--- etc/src/main/kotlin/etc/AtomicIntegerTest.kt
+++ etc/src/main/kotlin/etc/AtomicIntegerTest.kt
@@ -4,23 +4,37 @@ import java.util.concurrent.atomic.AtomicInteger
 
 private var count = 0
 
+// volatile -> 가시성 문제 해결
+// synchronized -> 가시성 + 원자성 해결 단 blocking 한다는 특징
+// atomic -> 가시성 + 원자성 해결하면서 non-blocking
+
 fun main() {
     val atomicCount = AtomicInteger(0)
+    val key = "key"
     val thread1 = Thread {
-        for (i in 0..99999) {
-            count++
-            atomicCount.incrementAndGet()
+        for (i in 0..999_999) {
+            synchronized(key) {
+                count++
+            }
+            atomicCount.getAndAdd(1)
         }
     }
     val thread2 = Thread {
-        for (i in 0..99999) {
-            count++
-            atomicCount.incrementAndGet()
+        for (i in 0..999_999) {
+            synchronized(key) {
+                count++
+            }
+            atomicCount.getAndAdd(1)
         }
     }
     thread1.start()
     thread2.start()
-    Thread.sleep(5000)
+    thread2.join()
+    thread1.join()
     println("atomic 결과 : " + atomicCount.get())
     println("int 결과 : $count")
+
+    val test1 = atomicCount.getAndAdd(10)
+    val test2 = atomicCount.incrementAndGet()
+    println("$test1 / $test2")
 }
--- etc/src/main/kotlin/etc/NotVolatileTest.kt
+++ etc/src/main/kotlin/etc/NotVolatileTest.kt
@@ -3,13 +3,20 @@ package etc
 private var stopRequested = false
 
 fun main() {
-    val Thread1 = Thread {
+    // JVM 상에서 Memory 위치는 같다.
+    // 사용을 할 때 CPU Cache 여기를 들린다.
+    val thread1 = Thread {
         var i = 0
-        while (!stopRequested) {
+        while (stopRequested.not()) {
             i++
         }
     }
-    Thread1.start()
-    Thread.sleep(1000)
+    thread1.start()
     stopRequested = true
+    Thread.sleep(1000)
 }
+
+/**
+ * 전 false 1609588821
+ * 후 true 1531448569
+ */
